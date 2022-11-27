ae519ebe5cdf952eed9e81dd32b88571013ccfb3
Paval-from-Belarus <star-war-sandakan@mail.ru>
2022-11-27 17:02:08 +0300

    update App.js

---
+++ src/locales/messages.js
@@ -0,0 +1,11 @@
+import {
+    LOCALES
+} from './locales';
+export const messages = {
+    [LOCALES.ENGLISH]:{
+        title: "The Greatest Title"
+    },
+    [LOCALES.RUSSIAN]:{
+        title: "Великий Заголовок"
+    }
+}
